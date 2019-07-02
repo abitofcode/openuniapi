f38882f7e3ade53fdbc92dbb7fb6f154863f6e7e
Christopher Wilson <chris@abitofcode.com>
2019-07-02 23:21:56 +0100

    Added remaining tests for lookup

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -1,8 +1,63 @@
 const frisby = require("frisby");
 
-it("should return a list of countries", function() {
-  // Return the Frisby.js Spec in the 'it()' (just like a promise)
-  return frisby
-    .get("http://localhost:8008/api/lookup/country")
-    .expect("status", 200);
+describe("Lookup", function() {
+  it("should return a list of random entries", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/random/thing")
+      .expect("status", 200);
+  });
+
+  it("should return a list of gender", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/gender")
+      .expect("status", 200);
+  });
+
+  it("should return a list of countries", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/country")
+      .expect("status", 200);
+  });
+
+  it("should return a list of enrolment status", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/enrolment_status")
+      .expect("status", 200);
+  });
+
+  it("should return a list of enrolment type", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/enrolment_type")
+      .expect("status", 200);
+  });
+
+  it("should return a list of attendance type", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/attendance_type")
+      .expect("status", 200);
+  });
+
+  it("should return a list of attendance reason", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/attendance_reason")
+      .expect("status", 200);
+  });
+
+  it("should return a list of ethnic origin", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/ethnic_origin")
+      .expect("status", 200);
+  });
+
+  it("should return a list of employment status", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/employment_status")
+      .expect("status", 200);
+  });
+
+  it("should return a list of occupations", function() {
+    return frisby
+      .get("http://localhost:8008/api/lookup/occupation")
+      .expect("status", 200);
+  });
 });
